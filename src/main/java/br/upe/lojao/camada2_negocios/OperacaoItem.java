5cf1211c7818a1c34b4d22b426c8cd3fe9e23129
Joao Flavio <joaoflaviolima53@gmail.com>
2026-06-22 22:33:28 -0300

    testando pra ver se as classes que sumiram da UI do eclipse voltam sendo que o git afirma que elas estão aqui quando uso git status

---
+++ src/main/java/br/upe/lojao/camada2_negocios/OperacaoItem.java
@@ -0,0 +1,29 @@
+package servicos;
+
+import br.upe.lojao.camada3_permanencia.Servicos;
+
+public class OperacaoItem implements Servicos {
+
+	private ArrayList<Produtos> listaProdutos;
+	private ArrayList<Categoria> listaCategoria;
+	private ArrayList<Fornecedor> listaFornecedor;
+	
+	public boolean cadastrar(Produtos item) {
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public int gerarID() {
+	    
+	}
+
+	@Override
+	public boolean verificarExclusão() {
+	    
+	}
+	
+}
---
+++ src/main/java/br/upe/lojao/camada3_persistencia/LeituraProdutos.java
@@ -0,0 +1,17 @@
+package br.upe.lojao.camada3_permanencia;
+
+public class LeituraProdutos {
+
+	private String caminhoProduto;
+	private String caminhoCategoria;
+	private String caminhofornecedor;
+	
+	public ArrayList<Produtos> lerProduto() {
+		
+	}
+	
+	public void atualizarProdutos(List<Produtos> lista) {
+		
+	}
+	
+}
