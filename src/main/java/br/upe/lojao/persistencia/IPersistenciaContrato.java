0a9a4c18b782eba1b0f14d2b71322cd6cbc75b5b
yan-dhsk <yanvianaaa@gmail.com>
2026-06-28 03:33:14 -0300

    feat: cria IPersistenciaContratos e PersistenciaContratos via LerCSV e EscreverCSV

--- src/main/java/br/upe/lojao/persistencia/IPersistenciaContrato.java
+++ src/main/java/br/upe/lojao/persistencia/IPersistenciaContrato.java
@@ -1,5 +1,28 @@
 package br.upe.lojao.persistencia;
 
-public interface IPersistenciaContratos {
+import java.util.ArrayList;
+import java.util.List;
+import br.upe.lojao.persistencia.entidades.Contrato;
+import br.upe.lojao.persistencia.entidades.Ocorrencias;
 
+public interface IPersistenciaContrato {
+    ArrayList<Contrato> lerContratos();
+    ArrayList<Ocorrencias> lerMultas();
+    int maiorIdContrato();
+    int maiorIdMulta();
+    Contrato buscarContrato(int id);
+    boolean adicionarContrato(Contrato contrato);
+    boolean atualizarContrato(Contrato contrato);
+    boolean atualizarContratos(List<Contrato> contratos);
+    boolean atualizarMultas(List<Ocorrencias> multas);
+    boolean deletarContrato(int id);
+    boolean itemContratoAtivo(int idItem);
+    boolean clienteMultaPendente(int idCliente);
+    boolean clienteHistorico(int idCliente);
+    List<Contrato> contratosAtrasados();
+    void recarregar();
+    Ocorrencias buscarMulta(int id);
+    boolean adicionarMulta(Ocorrencias multa);
+    boolean atualizarMulta(Ocorrencias multa);
+    boolean deletarMulta(int id);
 }
